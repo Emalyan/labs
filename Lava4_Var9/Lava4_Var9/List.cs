935885f2b0633bc6882c0260b86b916fad79b589
Ivan <brandy.bg@mail.ru>
2019-11-10 20:00:22 +0300

    Laba4 var 9

---
+++ Lava4_Var9/Lava4_Var9/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lava4_Var9
+{
+    public class List : List<string>
+    {
+        public static List operator +(List listOne, List listTwo)
+        {
+            var result = new List();
+            result.AddRange(listOne);
+            result.AddRange(listTwo);
+            return result;
+        }
+
+        public static bool operator !=(List listOne, List listTwo)
+        {
+            return true;
+        }
+
+        public static bool operator ==(List listOne, List listTwo)
+        {
+            return true;
+        }
+    }
+}
---
+++ Lava4_Var9/Lava4_Var9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lava4_Var9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOne = new List { "one", "two", "three" };
+            var listTwo = new List { "fore", "five" };
+            var result = listOne + listTwo;           
+            foreach(var m in listOne)
+            {
+                Console.WriteLine(m);
+            }
+
+            Console.ReadKey();
+        }                         
+    }
+}
